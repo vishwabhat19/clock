50afb4401e89f2276fa904d8948277a87fabeb86
Vishwa <VB086617>
2022-01-04 21:24:13 +0530

    Changes

--- clock/src/index.js
+++ clock/src/index.js
@@ -10,7 +10,7 @@ class App extends React.Component{
 
     componentDidMount(){
 
-    
+    //inside mount
         setInterval(() => {
             this.setState({
                 time: new Date().toLocaleTimeString()
